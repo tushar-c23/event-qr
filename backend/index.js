ead2032344be7e82ce44257cae980ebbc0cb7410
Priyanshu <agarwal3014@gmail.com>
2023-06-22 06:14:21 +0530

    Excel data stored in our database

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,33 @@
 const express = require("express");
 const app = express();
 const bp = require("body-parser");
-const qr = require("qrcode");
-
+const qr = require("qrcode"); 
+const Student=require("./student");
 app.set("view engine", "ejs");
-app.use(bp.urlencoded({extended: false}));
+app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 
-app.get("/", (req,res) => {
+Student(); 
+
+app.get("/", (req, res) => {
     res.render("index");
 })
 
-app.post("/scan/:id", (req,res) => {
+app.post("/scan/:id", (req, res) => {
     const { id } = req.params;
     console.log(id);
 })
 
-app.post("/generate", (req,res) => {
+app.post("/generate", (req, res) => {
     const url = req.body.url;
 
-    if(url.length === 0) res.send("Empty data");
+    if (url.length === 0) res.send("Empty data");
 
     qr.toDataURL(url, (err, src) => {
-        if(err) res.send("Error");
+        if (err) res.send("Error");
         console.log(src);
 
-        res.render("generate", {src});
+        res.render("generate", { src });
     });
 });
 
